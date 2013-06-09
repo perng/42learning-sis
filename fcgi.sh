9905b07fe48e45e6b275ab1cb1503449fdeb183e
Charles Perng <charles@perng.com>
2013-06-09 22:30:47 +0000

    tuition fee error fixed

--- fcgi.sh
+++ fcgi.sh
@@ -1,2 +1,2 @@
-python ./manage.py runfcgi port=8080 host=127.0.0.1 pidfile=sis.pid --settings=sis.settings
+python /root/sis/manage.py runfcgi port=8080 host=127.0.0.1 pidfile=sis.pid --settings=sis.settings
 
